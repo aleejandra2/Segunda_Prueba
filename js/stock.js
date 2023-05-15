af5ed73123295b4b598a53019f9a1eeeb6a2bce3
DiegoRosales123 <127795071+DiegoRosales123@users.noreply.github.com>
2023-05-14 20:23:15 -0400

    Se agrega carrito funcional

---
+++ js/stock.js
@@ -0,0 +1,11 @@
+let stockProductos = [
+    {id: 1, nombre: "The Weeknd", tipo: "album", cantidad: 1, desc: "Album Save Your Tears", precio: 14990, unidades: "Ultimas Unidades", img: 'imagen/weeknd.jpg'},
+    {id: 2, nombre: "Christian Castro", tipo: "album", cantidad: 1, desc: "Album Volver a Amar", precio: 9990, unidades: "Ultimas Unidades", img: 'imagen/christian_castro.webp'},
+    {id: 3, nombre: "Joytastic Sarah", tipo: "album", cantidad: 1, desc: "Album Fly Me To The Moon", precio: 12990, unidades: "Ultimas Unidades", img: 'imagen/fly_me_to_the_moon.jpg'},
+    {id: 4, nombre: "Rammstein", tipo: "album", cantidad: 1, desc: "Album de Sonne", precio: 9990, unidades: "Ultimas Unidades", img: 'imagen/images.jpg'},
+    {id: 5, nombre: "Kim Jonghyun", tipo: "album", cantidad: 1, desc: "Album Let Me Out", precio: 13500, unidades: "Ultimas Unidades", img: 'imagen/jonghyun.webp'},
+    {id: 6, nombre: "Madonna", tipo: "album", cantidad: 1, desc: "Album Hanky Panky", precio: 19990, unidades: "Ultimas Unidades", img: 'imagen/producto2.webp'},
+    {id: 7, nombre: "Elfen Lied", tipo: "album", cantidad: 1, desc: "Album Lilium", precio: 9990, unidades: "Ultimas Unidades", img: 'imagen/Lilium.webp'},
+    {id: 8, nombre: "Santa Feria", tipo: "album", cantidad: 1, desc: "Album Algun dia", precio: 9990, unidades: "Ultimas Unidades", img: 'imagen/santa_feria_algun_dia.jpg'},
+    {id: 9, nombre: "Reik", tipo: "album", cantidad: 1, desc: "Album Ciego", precio: 9990, unidades: "Ultimas Unidades", img: 'imagen/reik.webp'},
+]
